e792f960afec2b51cc76f813f7e6c25ab00b4688
unknown <islam15-3879@diu.edu.bd>
2022-10-27 18:10:17 +0600

    added

---
+++ 2.other.js
@@ -0,0 +1,22 @@
+
+// line 17 porjontw slhon local module 
+var name ="programming hero ";
+
+const add =(num1,num2)=>{
+    return num1+num2
+
+}
+
+//evabeo kora jay
+// module.exports.name=name;
+module.exports={
+    
+    name:name,
+    add:add
+
+}
+
+
+// module 3 prokar 
+// 1: core module jeta package e ace 
+// 2: local module jeta amara nijera touri kori 
