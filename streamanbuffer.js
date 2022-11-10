15e4d41c43c361dfeeb5fae68db5f826c876efea
Jahidul Islam <jahidulislam3879@gmail.com>
2022-11-10 12:57:15 +0600

    added

--- streamanbuffer.js
+++ streamanbuffer.js
@@ -9,3 +9,18 @@ readstream.on("data",(chunk)=>{
     console.log('..............');
     console.log(chunk)
 })
+
+readstream.on('open',()=>{
+    console.log("stream data is open")
+})
+setTimeout(()=>{
+readstream.pause('pause');
+console.log("steaming is pause now")
+
+},15)
+
+setTimeout(()=>{
+    readstream.resume()
+    console.log("streaming is resume now ")
+
+},8000)
