353e7d906194dbe5a5d1f3ebce57d259141d4ca6
Jahidul Islam <jahidulislam3879@gmail.com>
2022-11-10 12:35:06 +0600

    added

---
+++ event.js
@@ -0,0 +1,12 @@
+const event =require('events')
+
+
+// so prothome event create korte hobe 
+// const eventEmitter =new event.EventEmitter()
+// creating and event 
+
+const chitkarDibo =(){
+    console.log("oi beta ami dharaiya aci ");
+}
+
+// assign the handler into an event 
